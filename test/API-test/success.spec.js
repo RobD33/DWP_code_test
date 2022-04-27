8060f33e308fefedf1b70ec6c1c981b742853d44
Robert Davidson <robert.davidson@engineering.digital.dwp.gov.uk>
2022-04-27 15:51:36 +0100

    add first API test

---
+++ test/API-test/success.spec.js
@@ -0,0 +1,11 @@
+const request = require('supertest');
+const app = require('../../src/app');
+
+describe('GET /users', () => {
+  it('returns with status 200', () => {
+    return request(app).get('/users')
+      .then(response => {
+        expect(response.status).toEqual(200);
+      });
+  })
+});
