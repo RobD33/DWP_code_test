f8edbf8b594d7529c83a32ffab96d9a1741f12cd
Robert Davidson <robert.davidson@engineering.digital.dwp.gov.uk>
2022-04-27 16:54:00 +0100

    fix linting

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
 const app = require('./src/app');
-const port = process.env.PORT || 3000
-app.listen(port, () => console.log(`App listening on port ${port}`));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`App listening on port ${port}`)); // eslint-disable-line
--- test/API-test/success.spec.js
+++ test/API-test/success.spec.js
@@ -2,10 +2,8 @@ const request = require('supertest');
 const app = require('../../src/app');
 
 describe('GET /users', () => {
-  it('returns with status 200', () => {
-    return request(app).get('/users')
-      .then(response => {
-        expect(response.status).toEqual(200);
-      });
-  })
+  it('returns with status 200', () => request(app).get('/users')
+    .then((response) => {
+      expect(response.status).toEqual(200);
+    }));
 });
