74c5ab3a3f05ac835c0825539555ce230eca6284
Robert Davidson <robert.davidson@engineering.digital.dwp.gov.uk>
2022-04-28 17:53:16 +0100

    add test to add logger to req object

--- test/middleware/logger.spec.js
+++ test/middleware/logger.spec.js
@@ -4,4 +4,14 @@ describe('middleware logger', () => {
   it('is a function', () => {
     expect(typeof logger).toEqual('function');
   });
+
+  it('adds a logger to the request object', () => {
+    const req = {};
+    const res = {};
+    const next = jest.fn();
+
+    logger(req, res, next);
+    expect(req.logger).toBeTruthy();
+    expect(next).toHaveBeenCalled();
+  });
 });
