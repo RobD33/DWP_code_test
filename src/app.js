6c0b5cee3f4818781f7d8e5cc3556c8818fe09a5
Robert Davidson <robert.davidson@engineering.digital.dwp.gov.uk>
2022-04-28 20:59:25 +0100

    use logger in app

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const logger = require('./middleware/logger');
 
 const app = express();
 
+app.use(logger);
 app.use('/users', (req, res) => res.send({}));
 
 module.exports = app;
