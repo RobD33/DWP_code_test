787cc3dedd698fc8d71329aaef686653d7a083a9
Robert Davidson <robert.davidson@engineering.digital.dwp.gov.uk>
2022-04-28 23:56:12 +0100

    test that usersConttroller sends response

--- test/controllers/users.spec.js
+++ test/controllers/users.spec.js
@@ -1,7 +1,17 @@
 const usersController = require('../../src/controllers/users');
 
 describe('usersRouter', () => {
+  const req = {};
+  const res = {
+    send: jest.fn(),
+  };
+  const next = jest.fn()
   it('is a function', () => {
     expect(typeof usersController).toEqual('function');
   });
+
+  it('sends a response', () => {
+    usersController(req, res, next);
+    expect(res.send).toHaveBeenCalled();
+  })
 });
