2516cc9e13c0e11de4bf530751ca1ab9bd21ba4c
Robert Davidson <robert.davidson@engineering.digital.dwp.gov.uk>
2022-04-28 22:40:58 +0100

    add swagger endpoint to router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,11 @@
 const router = require('express').Router();
+const swaggerUi = require('swagger-ui-express');
+const yamljs = require('yamljs');
+const path = require('path');
+const filePath = path.join(__dirname, '../../docs/swagger.yaml');
+const swaggerDocument = yamljs.load(filePath);
 
 router.use('/users', (req, res) => res.send({}));
+router.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
