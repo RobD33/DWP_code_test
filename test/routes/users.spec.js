b368d65aa459487b0d9d363759c613b32f6e5a91
Robert Davidson <robert.davidson@engineering.digital.dwp.gov.uk>
2022-04-28 23:23:43 +0100

    add test for get method on / usersRouter

--- test/routes/users.spec.js
+++ test/routes/users.spec.js
@@ -4,4 +4,11 @@ describe('usersRouter', () => {
   it('is a function', () => {
     expect(typeof usersRouter).toEqual('function');
   });
+
+  it('has a / route with a get method', () => {
+    const route = usersRouter.stack.find((layer) => '/'.match(layer.regexp));
+    const { method } = route.route.stack[0];
+    expect(route).toBeTruthy();
+    expect(method).toEqual('get');
+  });
 });
