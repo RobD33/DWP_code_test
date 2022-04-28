d09f204cd188fbdf77e8825d8d30d99b3e543629
Robert Davidson <robert.davidson@engineering.digital.dwp.gov.uk>
2022-04-29 00:45:08 +0100

    add test for axios call in getUsers

--- src/clients/users.js
+++ src/clients/users.js
@@ -1,3 +1,12 @@
-const getUsers = () => {};
+require('dotenv').config();
+const axios = require('axios');
+
+const { TEST_API_URL } = process.env;
+
+const getUsers = () => {
+  const url = `${TEST_API_URL}/users`;
+  return axios.get(url)
+    .then((res) => res.data);
+};
 
 module.exports = { getUsers };
--- test/clients/users.spec.js
+++ test/clients/users.spec.js
@@ -1,14 +1,24 @@
+require('dotenv').config();
+const axios = require('axios');
 const { getUsers } = require('../../src/clients/users');
+
+jest.mock('axios', () => ({
+  get: jest.fn(() => Promise.resolve({ data: 'some data' })),
+}));
 
 describe('getUsers', () => {
   it('is a function', () => {
     expect(typeof getUsers).toEqual('function');
   });
 
-  it('returns a promise', () => {
-    return getUsers()
-      .then((response) => {
-        expect(response).toBeTruthy();
-      });
-  });
+  it('returns a promise', () => getUsers()
+    .then((response) => {
+      expect(response).toBeTruthy();
+    }));
+
+  it('sends a get request to test app users endpoint', () => getUsers()
+    .then((response) => {
+      expect(axios.get).toHaveBeenCalledWith(`${process.env.TEST_API_URL}/users`);
+      expect(response).toEqual('some data');
+    }));
 });
