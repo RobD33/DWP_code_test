5bcdd52e47d5351f211336abd739fe0b620cee5f
Robert Davidson <robert.davidson@engineering.digital.dwp.gov.uk>
2022-04-28 17:23:39 +0100

    fix linting

--- test/middleware/logger.spec.js
+++ test/middleware/logger.spec.js
@@ -2,6 +2,6 @@ const logger = require('../../src/middleware/logger');
 
 describe('middleware logger', () => {
   it('is a function', () => {
-    expect(typeof logger).toEqual('function')
-  })
-})
+    expect(typeof logger).toEqual('function');
+  });
+});
