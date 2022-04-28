0d8a15cb48a6d7fac44213875517be87c5e2938f
Robert Davidson <robert.davidson@engineering.digital.dwp.gov.uk>
2022-04-28 20:58:48 +0100

    add test for correlationId in response headers

--- test/API-test/success.spec.js
+++ test/API-test/success.spec.js
@@ -6,4 +6,11 @@ describe('GET /users', () => {
     .then((response) => {
       expect(response.status).toEqual(200);
     }));
+
+  it('returns a correlation-id', () => request(app)
+  .get('/users')
+  .set('correlation-id', 'a correlation id')
+  .then((response) => {
+    expect(response.header['correlation-id']).toEqual('a correlation id');
+  }));
 });
