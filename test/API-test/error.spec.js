7fba281e80dd06ae7b7fe4e208d780d24285122b
Robert Davidson <robert.davidson@engineering.digital.dwp.gov.uk>
2022-04-28 23:36:25 +0100

    add API test for 404

---
+++ test/API-test/error.spec.js
@@ -0,0 +1,10 @@
+const request = require('supertest');
+const app = require('../../src/app');
+
+describe('GET /users', () => {
+  it('returns with status 404', () => request(app).get('/somethingWierd')
+  .then((response) => {
+    expect(response.status).toEqual(404);
+    expect(response.body).toEqual({ message: 'Page not found'});
+  }));
+});
