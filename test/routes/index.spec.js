769ed4d7b022dd54f936195332e4e75fff3a2090
Robert Davidson <robert.davidson@engineering.digital.dwp.gov.uk>
2022-04-28 22:13:53 +0100

    test router for users endpoint

--- test/routes/index.spec.js
+++ test/routes/index.spec.js
@@ -4,4 +4,9 @@ describe('router', () => {
   it('is a function', () => {
     expect(typeof router).toEqual('function');
   });
+
+  it('has a /users route', () => {
+    const route = router.stack.find((layer) => '/users'.match(layer.regexp));
+    expect(route).toBeTruthy();
+  });
 });
