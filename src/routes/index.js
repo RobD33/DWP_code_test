be1c7c2ff67d6eabbb563c63711331f6e2758790
Robert Davidson <robert.davidson@engineering.digital.dwp.gov.uk>
2022-04-28 22:58:23 +0100

    add 404 to router

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
 const yamljs = require('yamljs');
 const path = require('path');
+const notFound = require('./404');
 
 const filePath = path.join(__dirname, '../../docs/swagger.yaml');
 const swaggerDocument = yamljs.load(filePath);
 
 router.use('/users', (req, res) => res.send({}));
 router.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+router.use('/*', notFound);
 
 module.exports = router;
