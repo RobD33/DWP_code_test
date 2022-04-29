4de8d3c3ccaa0924240eea1d612f95a4fcc5c4b4
Robert Davidson <robert.davidson@engineering.digital.dwp.gov.uk>
2022-04-29 01:30:41 +0100

    test filterUsers returns user within 50 miles of coords

--- test/helpers/filterUsers.spec.js
+++ test/helpers/filterUsers.spec.js
@@ -4,4 +4,18 @@ describe('filterUsers', () => {
   it('is a function', () => {
     expect(typeof filterUsers).toEqual('function');
   });
+
+  it('returns a user near london in an array', () => {
+    const london = { latitude: 51.5074, longitude: 0.1272 }; // source: https://www.google.com/search?q=centre+london+coordinates
+    const distance = 80467.2; // 50 miles in meters
+    const users = [
+      {
+        id: 1,
+        latitude: 51.4,
+        longitude: 0.1,
+      },
+    ];
+    const actual = filterUsers(users, london, distance);
+    expect(actual).toEqual(users);
+  });
 });
