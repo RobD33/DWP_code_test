35b2718b94bdde6b70b33c3f38f3c4f9e72fb2f7
Robert Davidson <robert.davidson@engineering.digital.dwp.gov.uk>
2022-04-29 01:59:40 +0100

    add test for uniqueUsers function

---
+++ test/helpers/uniqueUsers.spec.js
@@ -0,0 +1,7 @@
+const uniqueUsers = require('../../src/helpers/uniqueUsers');
+
+describe('uniqueUsers', () => {
+  it('is a function', () => {
+    expect(typeof uniqueUsers).toEqual('function');
+  });
+})
