065b64e4e98ca18ed8fc62ec23b7803c9bcb984c
Robert Davidson <robert.davidson@engineering.digital.dwp.gov.uk>
2022-04-29 01:40:25 +0100

    test fileredUsers can filter users

--- test/helpers/filterUsers.spec.js
+++ test/helpers/filterUsers.spec.js
@@ -18,4 +18,20 @@ describe('filterUsers', () => {
     const actual = filterUsers(users, london, distance);
     expect(actual).toEqual(users);
   });
+
+  it('filters out a user and returns a user near london in an array', () => {
+    const london = { latitude: 51.5074, longitude: 0.1272 }; // source: https://www.google.com/search?q=centre+london+coordinates
+    const distance = 80467.2; // 50 miles in meters
+    const userOne = {
+      latitude: 51.4,
+      longitude: 0.1,
+    };
+    const userTwo = {
+      latitude: 100,
+      longitude: 100,
+    };
+    const users = [ userOne, userTwo ];
+    const actual = filterUsers(users, london, distance);
+    expect(actual).toEqual([ userOne ]);
+  });
 });
