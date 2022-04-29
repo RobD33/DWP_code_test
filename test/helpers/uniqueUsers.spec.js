28370c199c776e60261a0d101b4e3406285924e2
Robert Davidson <robert.davidson@engineering.digital.dwp.gov.uk>
2022-04-29 02:03:48 +0100

    test uniqueUsers returns a user in an array

--- test/helpers/uniqueUsers.spec.js
+++ test/helpers/uniqueUsers.spec.js
@@ -4,4 +4,11 @@ describe('uniqueUsers', () => {
   it('is a function', () => {
     expect(typeof uniqueUsers).toEqual('function');
   });
+
+  it('returns a user in an array', () => {
+    const user = { id: 1};
+    const users = [user]
+    const actual = uniqueUsers(users)
+    expect(actual).toEqual(users);
+  });
 });
