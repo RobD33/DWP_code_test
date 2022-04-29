de909386a8653108e21076d1d1fc985384be5d6d
Robert Davidson <robert.davidson@engineering.digital.dwp.gov.uk>
2022-04-29 03:44:52 +0100

    add API test for 500 error

--- test/API-test/error.spec.js
+++ test/API-test/error.spec.js
@@ -1,5 +1,8 @@
 const request = require('supertest');
+const axios = require('axios');
 const app = require('../../src/app');
+
+jest.mock('axios');
 
 describe('GET /users', () => {
   it('returns with status 404', () => request(app).get('/somethingWierd')
@@ -7,4 +10,13 @@ describe('GET /users', () => {
       expect(response.status).toEqual(404);
       expect(response.body).toEqual({ message: 'Page not found' });
     }));
+
+  it('responds with status 500 for sever error', () => {
+    axios.get.mockImplementation(() => Promise.reject(new Error('an error')));
+    return request(app).get('/users')
+      .then((response) => {
+        expect(response.status).toEqual(500);
+        expect(response.body).toEqual({ message: 'Page not found' });
+      })
+  })
 });
