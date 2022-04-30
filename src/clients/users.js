beb914a93472285f05cbbb1e739dcc5ec83051ab
Rob Davidson <r.a.davidson@live.co.uk>
2022-04-30 15:15:33 +0200

    add logging to getUsersInLondon

--- src/clients/users.js
+++ src/clients/users.js
@@ -14,13 +14,63 @@ const getUsers = (logger) => {
         request_time: (Date.now() - startTime) / 1000,
       });
       return res.data;
+    })
+    .catch((err) => {
+      const request_time = (Date.now() - startTime) / 1000; // eslint-disable-line camelcase
+      if (err.response) {
+        const { data } = err.response;
+        logger.error({
+          api: '/users',
+          status: err.response.status,
+          request_time, // eslint-disable-line camelcase
+          message: (typeof data === 'string') ? data : JSON.stringify(data),
+        });
+      } else {
+        logger.error({
+          api: '/users',
+          status: 666,
+          request_time, // eslint-disable-line camelcase
+          error_code: err.code,
+          message: err.message,
+        });
+      }
+      throw err;
     });
 };
 
-const getUsersInLondon = () => {
+const getUsersInLondon = (logger) => {
+  const startTime = Date.now();
   const url = `${TEST_API_URL}city/London/users`;
   return axios.get(url)
-    .then((res) => res.data);
+    .then((res) => {
+      logger.info({
+        api: '/city/London/users',
+        status: res.status,
+        request_time: (Date.now() - startTime) / 1000,
+      });
+      return res.data;
+    })
+    .catch((err) => {
+      const request_time = (Date.now() - startTime) / 1000; // eslint-disable-line camelcase
+      if (err.response) {
+        const { data } = err.response;
+        logger.error({
+          api: '/city/London/users',
+          status: err.response.status,
+          request_time, // eslint-disable-line camelcase
+          message: (typeof data === 'string') ? data : JSON.stringify(data),
+        });
+      } else {
+        logger.error({
+          api: '/city/London/users',
+          status: 666,
+          request_time, // eslint-disable-line camelcase
+          error_code: err.code,
+          message: err.message,
+        });
+      }
+      throw err;
+    });
 };
 
 module.exports = { getUsers, getUsersInLondon };
--- test/clients/users.spec.js
+++ test/clients/users.spec.js
@@ -2,15 +2,15 @@ require('dotenv').config();
 const axios = require('axios');
 const { getUsers, getUsersInLondon } = require('../../src/clients/users');
 
-jest.mock('axios', () => ({
-  get: jest.fn(() => Promise.resolve({ status: 200, data: 'some data' })),
-}));
+jest.mock('axios');
 
 const logger = {
   info: jest.fn(),
+  error: jest.fn(),
 };
 
 describe('getUsers', () => {
+  axios.get.mockResolvedValue({ status: 200, data: 'some data' });
   it('is a function', () => {
     expect(typeof getUsers).toEqual('function');
   });
@@ -26,25 +26,85 @@ describe('getUsers', () => {
       expect(response).toEqual('some data');
     }));
 
-  it('logs info about the call', () => getUsers(logger)
+  it('logs info about a successful call', () => getUsers(logger)
     .then(() => {
-      expect(logger.info).toHaveBeenCalledWith({ api: '/users', request_time: 0, status: 200 });
+      expect(logger.info).toHaveBeenCalledWith({ api: '/users', request_time: expect.anything(), status: 200 });
     }));
+
+  it('logs info about a unsuccessful call', () => {
+    axios.get.mockRejectedValueOnce({ response: { data: 'not found', status: 404 } });
+    getUsers(logger)
+      .catch(() => {
+        expect(logger.error).toHaveBeenCalledWith({
+          api: '/users',
+          message: 'not found',
+          request_time: expect.anything(),
+          status: 404,
+        });
+      });
+  });
+
+  it('logs info about a connection error', () => {
+    axios.get.mockRejectedValueOnce({ code: 'ENOTFOUND', message: 'not found' });
+    getUsers(logger)
+      .catch(() => {
+        expect(logger.error).toHaveBeenCalledWith({
+          api: '/users',
+          error_code: 'ENOTFOUND',
+          message: 'not found',
+          request_time: expect.anything(),
+          status: 666,
+        });
+      });
+  });
 });
 
 describe('getUsersInLondon', () => {
+  axios.get.mockResolvedValue({ status: 200, data: 'some data' });
   it('is a function', () => {
     expect(typeof getUsersInLondon).toEqual('function');
   });
 
-  it('returns a promise', () => getUsersInLondon()
+  it('returns a promise', () => getUsersInLondon(logger)
     .then((response) => {
       expect(response).toBeTruthy();
     }));
 
-  it('sends a get request to test app users endpoint', () => getUsersInLondon()
+  it('sends a get request to test app users endpoint', () => getUsersInLondon(logger)
     .then((response) => {
       expect(axios.get).toHaveBeenCalledWith(`${process.env.TEST_API_URL}city/London/users`);
       expect(response).toEqual('some data');
     }));
+
+  it('logs info about a successful call', () => getUsersInLondon(logger)
+    .then(() => {
+      expect(logger.info).toHaveBeenCalledWith({ api: '/city/London/users', request_time: expect.anything(), status: 200 });
+    }));
+
+  it('logs info about a unsuccessful call', () => {
+    axios.get.mockRejectedValueOnce({ response: { data: 'not found', status: 404 } });
+    getUsersInLondon(logger)
+      .catch(() => {
+        expect(logger.error).toHaveBeenCalledWith({
+          api: '/city/London/users',
+          message: 'not found',
+          request_time: expect.anything(),
+          status: 404,
+        });
+      });
+  });
+
+  it('logs info about a connection error', () => {
+    axios.get.mockRejectedValueOnce({ code: 'ENOTFOUND', message: 'not found' });
+    getUsersInLondon(logger)
+      .catch(() => {
+        expect(logger.error).toHaveBeenCalledWith({
+          api: '/city/London/users',
+          error_code: 'ENOTFOUND',
+          message: 'not found',
+          request_time: expect.anything(),
+          status: 666,
+        });
+      });
+  });
 });
