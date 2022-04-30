affa5ee5753f1494f37135c1a37842ce5766569d
Rob Davidson <r.a.davidson@live.co.uk>
2022-04-30 15:58:27 +0200

    refactor clients/users for DRY

--- src/clients/users.js
+++ src/clients/users.js
@@ -3,13 +3,13 @@ const axios = require('axios');
 
 const { TEST_API_URL } = process.env;
 
-const getUsers = (req) => {
+const callAPI = (endpoint, req) => {
   const startTime = Date.now();
-  const url = `${TEST_API_URL}users`;
+  const url = `${TEST_API_URL}${endpoint}`;
   return axios.get(url)
     .then((res) => {
       req.logger.info({
-        api: '/users',
+        api: `/${endpoint}`,
         status: res.status,
         request_time: (Date.now() - startTime) / 1000,
         correlationId: req.correlationId,
@@ -21,7 +21,7 @@ const getUsers = (req) => {
       if (err.response) {
         const { data } = err.response;
         req.logger.error({
-          api: '/users',
+          api: `/${endpoint}`,
           status: err.response.status,
           request_time, // eslint-disable-line camelcase
           message: (typeof data === 'string') ? data : JSON.stringify(data),
@@ -29,7 +29,7 @@ const getUsers = (req) => {
         });
       } else {
         req.logger.error({
-          api: '/users',
+          api: `/${endpoint}`,
           status: 666,
           request_time, // eslint-disable-line camelcase
           error_code: err.code,
@@ -41,42 +41,7 @@ const getUsers = (req) => {
     });
 };
 
-const getUsersInLondon = (req) => {
-  const startTime = Date.now();
-  const url = `${TEST_API_URL}city/London/users`;
-  return axios.get(url)
-    .then((res) => {
-      req.logger.info({
-        api: '/city/London/users',
-        status: res.status,
-        request_time: (Date.now() - startTime) / 1000,
-        correlationId: req.correlationId,
-      });
-      return res.data;
-    })
-    .catch((err) => {
-      const request_time = (Date.now() - startTime) / 1000; // eslint-disable-line camelcase
-      if (err.response) {
-        const { data } = err.response;
-        req.logger.error({
-          api: '/city/London/users',
-          status: err.response.status,
-          request_time, // eslint-disable-line camelcase
-          message: (typeof data === 'string') ? data : JSON.stringify(data),
-          correlationId: req.correlationId,
-        });
-      } else {
-        req.logger.error({
-          api: '/city/London/users',
-          status: 666,
-          request_time, // eslint-disable-line camelcase
-          error_code: err.code,
-          message: err.message,
-          correlationId: req.correlationId,
-        });
-      }
-      throw err;
-    });
-};
+const getUsers = (req) => callAPI('users', req);
+const getUsersInLondon = (req) => callAPI('city/London/users', req);
 
 module.exports = { getUsers, getUsersInLondon };
